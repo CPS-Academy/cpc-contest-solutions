e40a6c288104a54419ce76b6238d31ac441dc979
Jahin Mahruf <jmsmahruf@gmail.com>
2026-04-13 12:33:23 +0600

    Update 01.Beautiful Sequence.cpp

--- Module-01/Vjudge/07.Bruteforce/01.Beautiful Sequence.cpp
+++ Module-01/Vjudge/07.Bruteforce/01.Beautiful Sequence.cpp
@@ -1,4 +1,21 @@
-#include<stdio.h>
-int main(){
-   //Solution not updated yet
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    while(T--) {
+        int m;
+        scanf("%d", &m);
+        int value;
+        bool isBeatiful = false;
+        for(int index = 1; index <= m; index++) {
+            scanf("%d", &value);
+            if(value <= index) isBeatiful = true;
+        }
+        if(isBeatiful) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+
+    return 0;
 }
