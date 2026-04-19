838079e7ed18d6307afb9e2456aa64f9b2dde198
Jahin Mahruf <jmsmahruf@gmail.com>
2026-04-19 17:45:19 +0600

    Update 04.Counting Pretty Numbers.cpp

--- Module-01/Vjudge/07.Bruteforce/04.Counting Pretty Numbers.cpp
+++ Module-01/Vjudge/07.Bruteforce/04.Counting Pretty Numbers.cpp
@@ -1,4 +1,18 @@
-#include<stdio.h>
-int main(){
-   //Solution not updated yet
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    while (T--) {
+        int l, r;
+        scanf("%d%d", &l, &r);
+        int cnt = 0;
+        for (int i = l; i <= r; i++) {
+            if (i % 10 == 2 || i % 10 == 3 || i % 10 == 9) {
+                cnt++;
+            }
+        }
+        printf("%d\n", cnt);
+    }
 }
