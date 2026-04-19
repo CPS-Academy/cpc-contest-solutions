b76d337b08bba99bf582d48c200218d5b4d4c032
Jahin Mahruf <jmsmahruf@gmail.com>
2026-04-19 17:44:39 +0600

    Update 03.Highest Divisor.cpp

--- Module-01/Vjudge/07.Bruteforce/03.Highest Divisor.cpp
+++ Module-01/Vjudge/07.Bruteforce/03.Highest Divisor.cpp
@@ -1,4 +1,15 @@
-#include<stdio.h>
-int main(){
-   //Solution not updated yet
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int ans = 1;
+    for (int i = 2; i <= 10; i++) {
+        if (n % i == 0) {
+            ans = i;
+        }
+    }
+    printf("%d\n", ans);
+    return 0;
 }
