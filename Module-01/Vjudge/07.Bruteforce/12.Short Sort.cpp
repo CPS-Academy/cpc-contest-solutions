cea89fae7737fcd9c4ba96c6ec9482b994e31b68
Jahin Mahruf <jmsmahruf@gmail.com>
2026-04-19 18:20:32 +0600

    Update 12.Short Sort.cpp

--- Module-01/Vjudge/07.Bruteforce/12.Short Sort.cpp
+++ Module-01/Vjudge/07.Bruteforce/12.Short Sort.cpp
@@ -1,4 +1,14 @@
-#include<stdio.h>
-int main(){
-   //Solution not updated yet
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int T;
+    scanf("%d", &T);
+    while (T--) {
+        char str[4];
+        scanf("%s", str);
+        if (strcmp(str, "bca") == false) printf("NO\n");
+        else if (strcmp(str, "cab") == false) printf("NO\n");
+        else printf("YES\n");
+    }
 }
